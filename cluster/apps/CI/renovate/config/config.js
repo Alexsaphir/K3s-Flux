eb46fc5b20dbb7fc5f24fe9e927c0f01829930dc
Alexsaphir <git@alexsaphir.com>
2023-05-09 23:17:45 +0200

    fix(renovate): fix docker host

--- cluster/apps/CI/renovate/config/config.js
+++ cluster/apps/CI/renovate/config/config.js
@@ -5,7 +5,6 @@ module.exports = {
     hostRules: [
     {
         hostType: 'docker',
-        "matchHost": "docker.io",
         username: process.env.DOCKER_HUB_USER,
         password: process.env.DOCKER_HUB_PASSWORD,
     },
