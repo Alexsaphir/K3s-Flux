637f6fdac00a06a507e7c746fe3a3ea71c3be91c
Alexsaphir <git@alexsaphir.com>
2023-07-19 22:36:29 +0200

    feat(ci): set flux-local scripts

---
+++ .gitlab/scripts/flux-local-diff.sh
@@ -0,0 +1,56 @@
+INPUTS_PATH=cluster
+INPUTS_SOURCES='cluster=./cluster/'
+
+
+LOG_LEVEL=INFO # DEBUG or INFO
+
+# Path to the root of the flux cluster GitRepository and Kustomization
+INPUTS_PATH=${INPUTS_PATH:?Variable not set or empty}
+
+# The name of the live branch to diff against, typically 'main' or 'master'.
+INPUTS_LIVE_BRANCH=${INPUTS_LIVE_BRANCH:-main}
+
+# Type of resource to diff, either 'kustomization' or 'helmrelease'.
+INPUTS_RESOURCE=${INPUTS_RESOURCE:-kustomization}
+
+# Number of diff lines to show.
+INPUTS_DIFF_LINES=${INPUTS_DIFF_LINES:-6}
+
+# When true, does not attempt to diff secrets.
+INPUTS_SKIP_SECRETS=${INPUTS_SKIP_SECRETS:-true}
+
+# When true, does not attempt to diff crds.
+INPUTS_CRDS=${INPUTS_CRDS:-true}
+
+# Kubernetes api versions used for helm Capabilities.APIVersions
+INPUTS_API_VERSIONS=${INPUTS_API_VERSIONS:-'policy/v1/PodDisruptionBudget'}
+
+# Labels and annotations that should be stripped to reduce diff noise.
+INPUTS_STRIP_ATTRS=${INPUTS_STRIP_ATTRS:-'helm.sh/chart,checksum/config,app.kubernetes.io/version,chart'}
+
+# Maximum number of bytes per individual diff (0=unlimited)
+INPUTS_LIMIT_BYTES=${INPUTS_LIMIT_BYTES:-10000}
+
+# Additional flags to pass to kustomize build
+INPUTS_KUSTOMIZE_BUILD_FLAGS=${INPUTS_KUSTOMIZE_BUILD_FLAGS:-''}
+
+# OCIRepository source mappings like `cluster=./kubernetes/`
+INPUTS_SOURCES=${INPUTS_SOURCES:-''}
+
+# matrix:
+#        path: ["kubernetes"]
+#        resource: ["helmrelease", "kustomization"]
+
+flux-local \
+  --log-level $LOG_LEVEL \
+  diff $INPUTS_RESOURCE \
+  --unified $INPUTS_DIFF_LINES \
+  --path ./$INPUTS_PATH \
+  --path-orig ../K3S-pr/$INPUTS_PATH \
+  --strip-attrs $INPUTS_STRIP_ATTRS \
+  --no-skip-crds \
+  --skip-secrets \
+  --limit-bytes $INPUTS_LIMIT_BYTES \
+  --all-namespaces \
+  --kustomize-build-flags="${INPUTS_KUSTOMIZE_BUILD_FLAGS}" \
+  --sources $INPUTS_SOURCES
---
+++ .gitlab/scripts/flux-local-test.sh
@@ -0,0 +1,40 @@
+INPUTS_PATH=cluster
+INPUTS_SOURCES='cluster=./cluster/'
+
+LOG_LEVEL=INFO # DEBUG or INFO
+
+# Path to the root of the flux cluster GitRepository and Kustomization.
+INPUTS_PATH=${INPUTS_PATH:?Variable not set or empty}
+
+# Build HelmRelease objects and validate outputs.
+INPUTS_ENABLE_HELM=${INPUTS_ENABLE_HELM:-true}
+
+# Kubernetes api versions used for helm Capabilities.APIVersions.
+INPUTS_API_VERSIONS=${INPUTS_API_VERSIONS:-'policy/v1/PodDisruptionBudget'}
+
+# Validate cluster resources against kyverno policies found in the cluster.
+INPUTS_ENABLE_KYVERNO=${INPUTS_ENABLE_KYVERNO:-false}
+
+# Kyverno version to use when enabled.
+INPUTS_KYVERNO_VERSION=${INPUTS_KYVERNO_VERSION:-v1.9.0}
+
+# Additional flags to pass to kustomize build.
+INPUTS_KUSTOMIZE_BUILD_FLAGS=${INPUTS_KUSTOMIZE_BUILD_FLAGS:-''}
+
+# OCIRepository source mappings like `cluster=./kubernetes/`.
+INPUTS_SOURCES=${INPUTS_SOURCES:-''}
+
+# matrix:
+#        path: ["kubernetes"]
+#        resource: ["helmrelease", "kustomization"]
+
+flux-local \
+  --log-level $LOG_LEVEL \
+  test \
+  --enable-helm \
+  --skip-secrets \
+  --no-enable-kyverno \
+  --api-versions "$INPUTS_API_VERSIONS" \
+  --kustomize-build-flags="$INPUTS_KUSTOMIZE_BUILD_FLAGS" \
+  --sources "$INPUTS_SOURCES" \
+  --path $INPUTS_PATH
