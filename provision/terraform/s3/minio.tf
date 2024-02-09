d762fbfc67975a8ef1db240dd01930536f440f6d
Alexsaphir <git@alexsaphir.com>
2024-02-10 00:39:57 +0100

    feat(minio): migrate codecov to local s3

--- provision/terraform/s3/minio.tf
+++ provision/terraform/s3/minio.tf
@@ -1,27 +1,50 @@
 locals {
-  minio_buckets = [
+  minio_buckets_nas = [
     "loki",
     "thanos",
     "backup-cloudnative",
     "backup-longhorn",
     "backup-volsync",
   ]
+  minio_buckets_k3s = [
+    "codecov",
+  ]
 }
 
-data "sops_file" "minio-creds" {
+data "sops_file" "minio-creds-nas" {
   source_file = "minio-nas.sops.yaml"
 }
 
-module "minio_bucket" {
-  for_each         = toset(local.minio_buckets)
+data "sops_file" "minio-creds-k3s" {
+  source_file = "minio-k3s.sops.yaml"
+}
+
+module "minio_bucket_nas" {
+  for_each         = toset(local.minio_buckets_nas)
   source           = "./modules/minio_bucket"
   bucket_name      = each.key
   is_public        = false
   owner_access_key = each.key
-  owner_secret_key = lookup(data.sops_file.minio-creds, "${each.key}_secret_key", null)
+  owner_secret_key = lookup(data.sops_file.minio-creds-nas, "${each.key}_secret_key", null)
   providers        = { minio = minio.nas }
 }
-output "minio_bucket_outputs" {
+
+output "minio_bucket_outputs_nas" {
+  value     = module.minio_bucket_nas
+  sensitive = true
+}
+
+module "minio_bucket" {
+  for_each         = toset(local.minio_buckets_k3s)
+  source           = "./modules/minio_bucket"
+  bucket_name      = each.key
+  is_public        = false
+  owner_access_key = each.key
+  owner_secret_key = lookup(data.sops_file.minio-creds-k3s, "${each.key}_secret_key", null)
+  providers        = { minio = minio.k3s }
+}
+
+output "minio_bucket_outputs_k3s" {
   value     = module.minio_bucket
   sensitive = true
 }
--- provision/terraform/s3/providers.tf
+++ provision/terraform/s3/providers.tf
@@ -1,6 +1,14 @@
 provider "minio" {
   alias          = "nas"
   minio_server   = "192.168.0.130:9768"
-  minio_user     = data.sops_file.minio-creds.data["root_access_key"]
-  minio_password = data.sops_file.minio-creds.data["root_secret_key"]
+  minio_user     = data.sops_file.minio-creds-nas.data["root_access_key"]
+  minio_password = data.sops_file.minio-creds-nas.data["root_secret_key"]
+}
+
+provider "minio" {
+  alias          = "k3s"
+  minio_server   = "s3.k3s.alexsaphir.com"
+  minio_ssl      = "true"
+  minio_user     = data.sops_file.minio-creds-k3s.data["root_access_key"]
+  minio_password = data.sops_file.minio-creds-k3s.data["root_secret_key"]
 }
