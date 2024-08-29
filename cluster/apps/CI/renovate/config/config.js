c9efc8b9cbbf4791be18f24289f2b1deaada507a
Alexsaphir <git@alexsaphir.com>
2024-08-29 18:34:39 +0200

    fix(renovate): replace deprecated flags, add missing

--- cluster/apps/CI/renovate/config/config.js
+++ cluster/apps/CI/renovate/config/config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    gitPrivateKey: process.env.RENOVATE_GIT_PRIVATE_KEY,
-    gitAuthor: process.env.RENOVATE_GIT_AUTHOR,
-
     requireConfig: 'required',
     onboardingNoDeps: 'auto',
     onboardingConfigFileName: '.gitlab/renovate.json5',
