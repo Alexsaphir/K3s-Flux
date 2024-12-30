e2c22d28a50445183f941874b0fe43160ea844f0
Alexsaphir <git@alexsaphir.com>
2024-12-30 21:34:28 +0100

    chore(provision): add cloudflare

---
+++ provision/terraform/cloudflare/setup.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.49.1"
+    }
+
+    sops = {
+      source  = "carlpett/sops"
+      version = "1.1.1"
+    }
+  }
+}
