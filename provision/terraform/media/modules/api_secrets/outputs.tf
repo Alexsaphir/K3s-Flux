8d66160d35b76da1eab7ace8d3ba431b8a5e50e1
Alexsaphir <git@alexsaphir.com>
2025-01-03 15:48:32 +0100

    fix(provision): use same variable than kubernetes

--- provision/terraform/media/modules/api_secrets/outputs.tf
+++ provision/terraform/media/modules/api_secrets/outputs.tf
@@ -23,6 +23,6 @@ output "ports" {
 
 output "domain_name" {
   description = "Domain name"
-  value       = data.kubernetes_secret.cluster-secrets.data["SECRET_CF_DOMAIN"]
+  value       = data.kubernetes_secret.cluster-secrets.data["DOMAIN_NAME"]
   sensitive   = true
 }
