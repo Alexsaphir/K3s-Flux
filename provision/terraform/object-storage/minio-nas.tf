61efb50792286108a88217e83089b8dd381352cc
Alexsaphir <git@alexsaphir.com>
2025-03-18 00:03:43 +0100

    feat(provision): add bucket for longhorn talos

--- provision/terraform/object-storage/minio-nas.tf
+++ provision/terraform/object-storage/minio-nas.tf
@@ -1,21 +1,22 @@
 variable "nas-buckets" {
   description = "List of Minio buckets to manage with `nas`."
-  type        = list(string)
+  type = list(string)
   default = [
     "loki",
     "thanos",
     "backup-cloudnative",
     "backup-longhorn",
+    "backup-longhorn-talos",
     "backup-volsync"
   ]
 }
 
 module "minio_buckets_nas" {
-  for_each         = toset(var.nas-buckets)
+  for_each = toset(var.nas-buckets)
   source           = "./modules/minio_bucket"
   bucket_name      = each.key
   is_public        = false
   owner_access_key = each.key
   owner_secret_key = data.sops_file.minio-creds-nas.data["${each.key}_secret_key"]
-  providers        = { minio = minio.nas }
+  providers = { minio = minio.nas }
 }
